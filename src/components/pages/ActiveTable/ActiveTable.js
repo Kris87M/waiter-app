0e8364321bf30dbca5f2720e65be084291406d47
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2024-10-13 21:46:32 +0200

    Update ActiveTable /implement table data editig and sync with API/

--- src/components/pages/ActiveTable/ActiveTable.js
+++ src/components/pages/ActiveTable/ActiveTable.js
@@ -1,29 +1,68 @@
 import CustomButton from "../../common/CustomButton";
-import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { getTableById } from "../../../redux/tablesRedux";
-import { Container, Form, Button } from "react-bootstrap";
-import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getTableById, updateTablesRequest } from "../../../redux/tablesRedux";
+import { Container, Form } from "react-bootstrap";
+import { useEffect, useState } from "react";
 
 const ActiveTable = () => {
   
   const { id } = useParams();
-
+  const navigate = useNavigate();
   const activeTable = useSelector(state => getTableById(state, id));
   // console.log(activeTable)
+  const dispatch = useDispatch();
   
   const [status, setStatus] = useState(activeTable.status);
   const [peopleAmount, setPeopleAmount] = useState(activeTable.peopleAmount);
   const [maxPeopleAmount, setMaxPeopleAmount] = useState(activeTable.maxPeopleAmount);
   const [bill, setBill] = useState(activeTable.bill);
 
+  useEffect(() => {
+    if (!activeTable) {
+      navigate('/');
+    }
+  }, [activeTable, navigate]);
+
+  useEffect(() => {
+    if (status === 'Free' || status === 'Cleaning') {
+      setPeopleAmount(0);
+    }
+    if (status !== 'Busy') {
+      setBill(0);
+    }
+  }, [status, setPeopleAmount]);
+
+  const handlePeopleAmount = value => {
+    setPeopleAmount(value > maxPeopleAmount ? maxPeopleAmount : Math.max(0, Math.min(value, 10)));
+  }
+
+  const handleMaxPeopleAmount = value => {
+    setMaxPeopleAmount(Math.max(0, Math.min(value, 10)));
+    if (peopleAmount > value) {
+      setPeopleAmount(value);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const updatedTable = {
+      id: activeTable.id,
+      status,
+      peopleAmount: peopleAmount > maxPeopleAmount ? maxPeopleAmount : peopleAmount,
+      bill: status === 'Busy' ? bill : 0,
+    }
+    dispatch(updateTablesRequest(updatedTable));
+    navigate('/');
+  }
+
   return (
     <Container>
       <h1>Table {activeTable.id}</h1>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="d-flex align-items-center my-3" controlId="status">
           <Form.Label className="me-3"><strong>Status:</strong></Form.Label>
-          <Form.Select value={status} style={{ maxWidth: 500 }}>
+          <Form.Select value={status} style={{ maxWidth: 500 }} onChange={e => setStatus(e.target.value)}>
             <option value="Free">Free</option>
             <option value="Reserved">Reserved</option>
             <option value="Busy">Busy</option>
@@ -34,16 +73,16 @@ const ActiveTable = () => {
         <Form.Group className="d-flex align-items-center mb-3" controlId="people">
           <Form.Label className="me-3"><strong>People:</strong></Form.Label>
           <div className="d-flex align-items-center">
-            <Form.Control value={ peopleAmount } style={{ maxWidth: 100 }} type="number"></Form.Control>
+            <Form.Control value={ peopleAmount } style={{ maxWidth: 100 }} type="number" disabled={status === 'Free' || status === 'Cleaning'} onChange={e => handlePeopleAmount(e.target.value)}></Form.Control>
             <span className="mx-2">/</span>
-            <Form.Control value={ maxPeopleAmount} style={{ maxWidth: 100 }} type="number"></Form.Control>
+            <Form.Control value={ maxPeopleAmount} style={{ maxWidth: 100 }} type="number" max={10} onChange={e => handleMaxPeopleAmount(e.target.value)}></Form.Control>
           </div>
         </Form.Group>
 
         <Form.Group className="d-flex align-items-center mb-3" controlId="bill">
           <Form.Label className="me-3"><strong>Bill:</strong></Form.Label>
           <span className="mx-2">$</span>
-          <Form.Control value={ bill } style={{ maxWidth: 100 }} type="number"></Form.Control>
+          <Form.Control value={ bill } style={{ maxWidth: 100 }} type="number" onChange={e => setBill(e.target.value)}></Form.Control>
         </Form.Group>
 
         <CustomButton type="submit">Update</CustomButton>
--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -5,22 +5,40 @@ export const getTableById = (state, id) => state.tables.find(table => table.id =
 // actions
 const createActionName = actionName => `app/tables/${actionName}`;
 const UPDATE_TABLES = createActionName('UPDATE_TABLES');
+const UPDATE_SINGLE_TABLE = createActionName('UPDATE_SINGLE_TABLE');
 
 // action creators
 export const updateTables = payload => ({ type: UPDATE_TABLES, payload });
+export const updateSingleTable = payload => ({ type: UPDATE_SINGLE_TABLE, payload });
 
 export const fetchTables = () => {
   return (dispatch) => {
     fetch('http://localhost:3131/api/tables')
-      .then(res => res.json())
-      .then(tables => dispatch(updateTables(tables)));
+    .then(res => res.json())
+    .then(tables => dispatch(updateTables(tables)));
   };
 };
+
+export const updateTablesRequest = (updatedTable) => {
+  return (dispatch) => {
+    const options = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedTable),
+    };
+    fetch(`http://localhost:3131/api/tables/${updatedTable.id}`, options)
+      .then(() => dispatch(updateSingleTable(updatedTable)))
+  }
+}
 
 const tablesReducer = (statePart = [], action) => {
   switch (action.type) {
     case UPDATE_TABLES:
       return [...action.payload];
+    case UPDATE_SINGLE_TABLE:
+      return statePart.map(table => table.id === action.payload.id ? action.payload : table);
     default:
       return statePart;
   };
